a678b6f8f99fe35ab98f7f426841ff4041d5df34
Patricia Gusen <p.gusen@gmail.com>
2020-07-21 17:36:50 -0300

    Melhorando a implementacao do metodo imprimir tabuleiro

--- chess/chess/Program.cs
+++ chess/chess/Program.cs
@@ -12,19 +12,17 @@ namespace chess
         {
             try
             {
-                //Tabuleiro tab = new Tabuleiro(8, 8);
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-                //tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                //tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                //tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 //tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-                //Tela.ImprimirTabuleiro(tab);
-
-                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-
-                Console.WriteLine(pos.ToPosicao());
-
-                //Console.WriteLine(pos);
+                Tela.ImprimirTabuleiro(tab);
             }
             catch(TabuleiroException e)
             {
